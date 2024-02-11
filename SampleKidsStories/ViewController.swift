b80b4ff66292758b286ea18eff982f4aa8db48ac
Smit Patel <smit.patel7585@gmail.com>
2024-02-11 17:28:44 -0500

    Initial Commit

---
+++ SampleKidsStories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleKidsStories
+//
+//  Created by user235217 on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
